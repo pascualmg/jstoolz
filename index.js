f58edbcad8d4fa4e94d92e87b607b8279c30047b
Pascual <pascual.munoz@pccomponentes.com>
2018-08-17 00:58:41 +0200

    all files

---
+++ index.js
@@ -0,0 +1,16 @@
+export default {
+  generateRandomId: function generateRamdomId (seedStr) {
+    const len = seedStr.length
+    console.log('possibleStr', seedStr, ' len ', len)
+    if (len <= 1) {
+      return seedStr
+    }
+    const randomValidIndex = Math.floor(Math.random() * len)
+    const randomChar = seedStr[randomValidIndex]
+    const chunk1 = seedStr.slice(0, randomValidIndex)
+    const chunk2 = seedStr.slice(randomValidIndex + 1)
+    const possibleStrWithoutRandomChar = chunk1.concat(chunk2)
+
+    return randomChar + generateRamdomId(possibleStrWithoutRandomChar)
+  }
+}
